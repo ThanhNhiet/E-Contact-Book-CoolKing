ffbe682851f2eaf6c178b9381de1bfb6b5271484
TranKeTuanVuong <tuanvuongpokap@gmail.com>
2025-10-06 17:52:57 +0700

    update

--- Coolking_Server/server.js
+++ Coolking_Server/server.js
@@ -13,7 +13,7 @@ app.use(express.json());
 
 // Bật cors cho phép frontend gọi API
 app.use(cors({
-  origin: "http://localhost:5173", // domain React (vite)
+  origin: "*", // domain React (vite)
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true, // nếu cần gửi cookie/token
